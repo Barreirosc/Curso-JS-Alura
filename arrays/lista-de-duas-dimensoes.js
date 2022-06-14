f44fbfdd8ece5096da1a4d4bb1aab3c1b05e41cf
Barreirosc <jpaullobr@gmail.com>
2022-06-14 15:29:09 -0300

    Atualizando desafios arrays

---
+++ arrays/lista-de-duas-dimensoes.js
@@ -0,0 +1,9 @@
+let listaAlunos = ['João', 'Juliana', 'Caio', 'Ana']
+
+let listaMediasAlunos = [10, 7, 9, 6]
+
+let listaDeNotasAlunos = [listaAlunos, listaMediasAlunos]
+
+console.log(
+  `${listaDeNotasAlunos[0][1]}, sua média é ${listaDeNotasAlunos[1][1]}`
+)
